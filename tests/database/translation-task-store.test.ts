39ac3cfe275d743526970323c980bb1cd13b25eb
iliya1947 <iliya1947a@gmail.com>
2026-09-25 14:42:18 +0300

    test: scope allowance migration to task-store schema

--- tests/database/translation-task-store.test.ts
+++ tests/database/translation-task-store.test.ts
@@ -38,7 +38,10 @@ beforeAll(async () => {
   await client.query(await readFile("drizzle/0010_translation_task_generation_order.sql", "utf8"));
   await client.query(await readFile("drizzle/0012_translation_task_retry_dlq.sql", "utf8"));
   await client.query(await readFile("drizzle/0013_translation_task_reconciliation.sql", "utf8"));
-  await client.query(await readFile("drizzle/0019_content_translation_allowance_admission.sql", "utf8"));
+  await client.query(
+    (await readFile("drizzle/0019_content_translation_allowance_admission.sql", "utf8"))
+      .replaceAll('"public".', `"${schemaName}".`),
+  );
 });
 
 afterAll(async () => {
